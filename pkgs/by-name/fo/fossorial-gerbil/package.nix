6a7e4d4217972e5c075fd0bb290c2d44b872136d
jack <github@jackr.eu>
2025-05-09 19:26:10 +0200

    patch usr/sbin

--- pkgs/by-name/fo/fossorial-gerbil/package.nix
+++ pkgs/by-name/fo/fossorial-gerbil/package.nix
@@ -1,6 +1,9 @@
 {
+  pkgs,
+  lib,
   fetchFromGitHub,
-  buildGoModule
+  buildGoModule,
+  replaceVars
 }:
 
 buildGoModule rec {
@@ -13,6 +16,21 @@ buildGoModule rec {
     tag = "${version}";
     hash = "sha256-6ZmnokXmn4KIfNZT9HrraYP4fjfY2C0sK+xAJyq/pkU=";
   };
+  # patch out the /usr/sbin/iptables
+  patches = [
+      (replaceVars ./iptables.patch {
+        iptables = lib.getExe pkgs.iptables;
+      })
+  ];
 
   vendorHash = "sha256-lYJjw+V94oxILu+akUnzGACtsU7CLGwljysRvyUk+yA=";
+
+  meta = {
+    description = "A simple WireGuard interface management server";
+    homepage = "https://github.com/fosrl/gerbil";
+    changelog = "https://github.com/fosrl/gerbil/releases/tag/${version}";
+    license = lib.licenses.agpl3Only;
+    maintainers = [ ];
+    platforms = lib.platforms.linux;
+  };
 }
