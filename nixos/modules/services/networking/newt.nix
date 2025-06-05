85d55ff1b3d70893b24f3ba8d3d3bea829740994
jack <github@jackr.eu>
2025-06-05 21:51:09 +0200

    finish newt

--- nixos/modules/services/networking/newt.nix
+++ nixos/modules/services/networking/newt.nix
@@ -12,7 +12,6 @@ in
   options = {
     services.newt = {
       enable = lib.mkEnableOption "Newt";
-      package = lib.mkPackageOption pkgs "newt" { };
       # provide path to file to keep secrets out of the nix store
       configFile = lib.mkOption {
         type = lib.types.nullOr lib.types.path;
@@ -40,32 +39,33 @@ in
     users.users.newt = {
       isSystemUser = true;
       group = "newt";
+      home = "/var/lib/newt";
     };
-
     users.groups.newt = {};
 
     systemd.services.newt = {
       description = "Newt Service";
       wantedBy = [ "multi-user.target" ];
       after = [ "network.target" ];
-
       serviceConfig = {
         User = "newt";
         Group = "newt";
+        StateDirectory = "newt";
+        WorkingDirectory = "%S/newt";
         Restart = "always";
         RestartSec = "10s";
         ExecStart =
-          let
-            newtConfig = builtins.fromJSON (builtins.readFile cfg.configFile);
-
-            args = [
-              (lib.getExe cfg.package)
-              "--id" newtConfig.id
-              "--secret" newtConfig.secret
-              "--endpoint" newtConfig.endpoint
-            ];
-          in
-          utils.escapeSystemdExecArgs args;
+        let
+          newtConfig = builtins.fromJSON (builtins.readFile cfg.configFile);
+        in
+        utils.escapeSystemdExecArgs (
+          [
+            (lib.getExe' pkgs.fosrl-newt "newt")
+            "--id" newtConfig.id
+            "--secret" newtConfig.secret
+            "--endpoint" newtConfig.endpoint
+          ]
+        );
       };
     };
   };
